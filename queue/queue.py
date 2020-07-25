3702e18383cecf14f532e34c6242a481f305217c
unknown <ken.brandon@aol.com>
2020-07-25 14:07:55 -0700

    queue

--- queue/queue.py
+++ queue/queue.py
@@ -1,8 +1,7 @@
-from Node import LinkedList
-from stack import Stack
 """
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order. 
+
 1. Implement the Queue class using an array as the underlying storage structure.
    Make sure the Queue tests pass.
 2. Re-implement the Queue class, this time using the linked list implementation
@@ -14,72 +13,54 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
-# Implementation using list
-# class Queue:
-#     def __init__(self):
-#         self.size = 0
-#         self.storage = []
 
-#     def __len__(self):
-#         return self.size
+# line in store
+# add to back
+# remove from front
 
-#     def enqueue(self, value):
-#         self.storage.append(value)
-#         self.size += 1
-
-#     def dequeue(self):
-#         if self.size is 0:
-#             return None
-#         else:
-#             val = self.storage[0]
-#             self.storage.remove(val)
-#             self.size -= 1
-#             return val
-
-# Implementation using LinkedList
-# class Queue:
-#     def __init__(self):
-#         self.size = 0
-#         self.storage = LinkedList()
-
-#     def __len__(self):
-#         return self.size
-
-#     def enqueue(self, value):
-#         self.storage.add_to_tail(value)
-#         self.size += 1
-
-#     def dequeue(self):
-#         if self.size is 0:
-#             return None
-#         else:
-#             val = self.storage.head.get_value()
-#             self.storage.remove_head()
-#             self.size -= 1
-#             return val
-
-# Implementation using Stacks - Stretch
+from singly_linked_list import LinkedList
 
 
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = Stack()
+        self.storage = LinkedList()
 
     def __len__(self):
         return self.size
 
-    def enqueue(self, value):
-        self.storage.push(value)
+    def enqueue(self, value):  # appending to the tail
+        self.storage.add_to_tail(value)
         self.size += 1
 
-    def dequeue(self):
-        if self.size is 0:
-            return None
-        elif self.size is 1:
-            return self.storage.pop()
-        else:
-            head = self.storage.head
-            self.storage.head = head.next_node
+    def dequeue(self):  # removing from the head
+        if self.size > 0:
+            removed = self.storage.remove_head()
             self.size -= 1
-            return head.get_value()
+            return removed
+        else:
+            return None
+
+
+# CALLING, WILL ENQUEUE AND DEQUEUE TO WORK LIKE A REVOLING DOOR/LINE
+
+# class Queue:
+
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+
+#     def enqueue(self, value):       # appending to the back
+#         self.storage.append(value)
+#         self.size += 1
+
+#     def dequeue(self):              # pop-ing from the front
+#         if self.storage == []:
+#             return None
+#         else:
+#             self.size -= 1
+#             return self.storage.pop(0)
